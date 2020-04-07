eefb41e57e767d1b73dabfaa7f76c7de3e2f5f7d
William Craig <craig@mac.local>
2020-04-07 18:59:46 -0500

    client side authentication with React

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+class Signup extends React.Component {
+  onSubmit = formProps => {
+    this.props.signup(formProps, () => {
+      this.props.history.push("/feature");
+    });
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label htmlFor="">Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="">Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div>{this.props.errorMessage}</div>
+        <button type="submit">Signup</button>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(
+  connect(
+    mapStateToProps,
+    actions
+  ),
+  reduxForm({ form: "signup" })
+)(Signup);
